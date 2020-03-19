e1be85c9aaf201b1942e243103d25de73074210e
W Group <48428504+wgroupdevs@users.noreply.github.com>
2020-03-19 11:14:08 +0500

    zohaib

--- WalahalaWalletSDK/WalahalaWallet/Program.cs
+++ WalahalaWalletSDK/WalahalaWallet/Program.cs
@@ -19,30 +19,30 @@ namespace WalahalaWallet
             ///Transaction object
             ITransaction transaction = new Transaction();
 
-              var res = transaction.GenerateWallet("host blur addict matter leg beyond super sausage hawk deny ring spin practice midnight gold spider mail shove oval raccoon object upon rescue rather report arm rubber output skate hold slam maze educate");
-              var res1 = transaction.GenerateWallet(" rotate notice logic silver pen live sting frozen defy latin ten noise enjoy figure");
-              var w =transaction.GetWallet(res.PrivateKey);
-             // var res =transaction.Test();
-             // var wall =transaction.GetWallet(res);
+             // var res = transaction.GenerateWallet("host blur addict matter leg beyond super sausage hawk deny ring spin practice midnight gold spider mail shove oval raccoon object upon rescue rather report arm rubber output skate hold slam maze educate");
+             // var res1 = transaction.GenerateWallet(" rotate notice logic silver pen live sting frozen defy latin ten noise enjoy figure");
+             // var w =transaction.GetWallet(res.PrivateKey);
+            // var res =transaction.Test();
+            // var wall =transaction.GetWallet(res);
 
 
 
-            ///// GENERATE WALLET
-            ///// 
-            //var wallet =transaction.GenerateWallet();
-            //Console.WriteLine(wallet.Address);
-            ///// GET BALANCE
-            ///// 
-            //var balance =transaction.GetBalance("WPGBc4quEmCUhA6DgJdbQBD4tA8zrYffqf", "http://coin.walahala.org/api/");
-            //Console.WriteLine(balance);
-            
-            ///// Generate a signed transaction ofline
-            //var trans =transaction.GenerateSignedTransaction(wallet.Mnemonics, "WPGBc4quEmCUhA6DgJdbQBD4tA8zrYffqf", 5);
-            //Console.WriteLine(trans);
+            /// GENERATE WALLET
+            /// 
+            var wallet = transaction.GenerateWallet("host blur addict matter leg beyond super sausage hawk deny ring spin practice midnight gold spider mail shove oval raccoon object upon rescue rather report arm rubber output skate hold slam maze educate");
+            Console.WriteLine(wallet.Address);
+            /// GET BALANCE
+            /// 
+            var balance = transaction.GetBalance("WPGBc4quEmCUhA6DgJdbQBD4tA8zrYffqf", "http://coin.walahala.org/api/");
+            Console.WriteLine(balance);
 
-            ////// Broadcast transaction to network
-            //var res =transaction.BroadCastTransaction(trans, "http://coin.walahala.org/api/");
-            //Console.WriteLine(res);
+            /// Generate a signed transaction ofline
+            var trans = transaction.GenerateSignedTransaction(wallet.Mnemonics, "WPGBc4quEmCUhA6DgJdbQBD4tA8zrYffqf", 1);
+            Console.WriteLine(trans);
+
+            //// Broadcast transaction to network
+            var res = transaction.BroadCastTransaction(trans, "http://coin.walahala.org/api/");
+            Console.WriteLine(res);
 
             Console.ReadKey();
         }
